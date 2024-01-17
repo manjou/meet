7cd6900360437ace56c8b08ee5af971903bf9ebc
Bernd Janzen <103428887+manjou@users.noreply.github.com>
2024-01-17 16:32:08 +0100

    meet app ex4.4. v.2.0.0. Tests scenario 2 fails

---
+++ src/__tests__/CitySearch.test.js
@@ -0,0 +1,34 @@
+// src/__tests__/CitySearch.test.js
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { render } from '@testing-library/react';
+import CitySearch from '../components/CitySearch';
+import { fireEvent } from '@testing-library/react';
+
+describe('<CitySearch /> component', () => {
+    let CitySearchComponent;
+    beforeEach(() => {
+        CitySearchComponent = render(<CitySearch />);
+    })
+  
+    test('renders text input', () => {
+    const cityTextBox = CitySearchComponent.queryByRole('textbox');
+    expect(cityTextBox).toBeInTheDocument();
+    expect(cityTextBox).toHaveClass('city');
+  });
+
+  test('suggestions list is hidden by default', () => {
+    const suggestionList = CitySearchComponent.queryByRole('list');
+    expect(suggestionList).not.toBeInTheDocument();
+  });
+
+  test('renders a list of suggestions when city textbox gains focus', async () => {
+    const user = userEvent.setup();
+    const cityTextBox = CitySearchComponent.queryByRole('textbox');
+    await user.click(cityTextBox);
+    const suggestionList = CitySearchComponent.queryByRole('list');
+    expect(suggestionList).toBeInTheDocument();
+    expect(suggestionList).toHaveClass('suggestions');
+  });
+
+});
